fa1a4d8ddc0d5c53c1678e5fa62428a8945cb9eb
jeyy2 <bdieye467@gmail.com>
2024-12-21 13:42:12 +0300

    Add files via upload

---
+++ uest.php
@@ -0,0 +1,7 @@
+echo "# jeyyNova" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/jeyy2/jeyyNova.git
+git push -u origin main
